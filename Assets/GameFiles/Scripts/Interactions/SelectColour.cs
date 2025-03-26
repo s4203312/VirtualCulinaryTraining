a2530f7302484fc88b735464a6c6f08aa0b1ce8a
Alec Robinson <s4203312@glos.ac.uk>
2025-03-26 14:57:15 +0000

    colour select

---
+++ Assets/GameFiles/Scripts/Interactions/SelectColour.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectColour : MonoBehaviour
+{
+    public Camera currentCam;
+    public GameObject hitColour;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            hitColour = ChooseColour();
+        }
+    }
+    private GameObject ChooseColour()
+    {
+        GameObject hitColour = null;
+
+        Ray ray = currentCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.Log(hit.transform.name);
+            hitColour = hit.collider.gameObject;
+        }
+
+        return hitColour;
+    }
+}
--- Assets/GameFiles/Scripts/Interactions/Slicing/MouseSlicing.cs
+++ Assets/GameFiles/Scripts/Interactions/Slicing/MouseSlicing.cs
@@ -1,23 +1,55 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MouseSlicing : MonoBehaviour
 {
     public GameObject knife;
+    public GameObject choppingBoard;
+    public GameObject UIcanvas;
+    public GameObject colourSelectionPrefab;
+    private GameObject colourSelection;
+
     private bool isKnifeInHand = false;
+    private bool isInteracting = false;
 
     private void Update()
     {
+        //Selecting colour of board
+        if(isInteracting == false && GetComponent<Camera>().enabled == true)
+        {
+            isInteracting = true;
+            ShowColourSelection();
+        }
+        if(colourSelection != null && colourSelection.GetComponent<SelectColour>().hitColour != null)
+        {
+            GameObject hitObject = colourSelection.GetComponent<SelectColour>().hitColour;
+            Material chosenMaterial = hitObject.GetComponent<Renderer>().material;
+            choppingBoard.GetComponent<Renderer>().material = chosenMaterial;
+            Destroy(colourSelection);
+
+            UIcanvas.SetActive(true);
+        }
+
+        //Picking up knife
         if (isKnifeInHand)
         {
-            Vector3 lookAtPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z) + transform.forward;
-            knife.transform.position = GetComponent<Camera>().ScreenToWorldPoint(lookAtPoint);
+            //Functionality for knife
         }
+    }
+
+    private void ShowColourSelection()
+    {
+        Vector3 newPos = this.transform.position + this.transform.forward * 0.8f;
+        colourSelection = Instantiate(colourSelectionPrefab, newPos, Quaternion.identity);
+        colourSelection.GetComponent<SelectColour>().currentCam = GetComponent<Camera>();
     }
 
     public void PickUpKnife()
     {
         isKnifeInHand = true;
-        //return knife.transform.position;
+        UIcanvas.SetActive(false);
+
+        knife.transform.position = this.transform.position + this.transform.forward * 0.8f;
     }
 
     public void DropKnife(Vector3 oldKnifePos)
