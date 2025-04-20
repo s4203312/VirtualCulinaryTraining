478c240fe160f184bd37076b50d76d90a4834e90
Alec Robinson <s4203312@glos.ac.uk>
2025-04-20 19:35:39 +0100

    found lettuce

--- Assets/GameFiles/Scripts/Feedback.cs
+++ Assets/GameFiles/Scripts/Feedback.cs
@@ -56,6 +56,17 @@ public class Feedback : MonoBehaviour
                                         + " chopping board. Next time use a " + correctBoard + " chopping board for the " + incorrectName;
                     }
                 }
+                if (anaEvent.eventName == "Sauces Event:")
+                {
+                    if (anaEvent.eventData[i] == "False")
+                    {
+                        //Retrieving the incorrect data
+                        incorrectName = anaEvent.eventData[i - 1].ToString();
+                        incorrectValue = anaEvent.eventData[i - 2].ToString();
+
+                        //Add feedback
+                    }
+                }
             }
         }
     }
